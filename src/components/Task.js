d0371dcb842e98850d49f358cdeb1414691a9e9e
KazutakaSakurai <kazutaka-sakurai@garage.co.jp>
2021-04-27 20:49:05 +0900

    add Task stories

--- src/components/Task.js
+++ src/components/Task.js
@@ -18,7 +18,7 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
                     value={title}
                     readOnly={true}
                     placeholder="Input title"
-                    style={{ background: 'teal' }}
+                    style={{ background: '#f7f7f7',textOverflow: 'ellipsis' }}
                 />
             </div>
 
